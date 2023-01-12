66b3fe8662ca5ce3af59d8834050b5e31b91eb6d
olawale <jao.olaniyi@gmail.com>
2023-01-12 09:34:56 +0100

    removed unused files css

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
+import { Flex, Box, Text, Button } from "@chakra-ui/react";
 
 export default function Home() {
-  return <></>;
+  return (
+    <div>
+      <h1>helo world jhjhgj</h1>
+    </div>
+  );
 }
