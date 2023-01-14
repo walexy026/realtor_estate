a152a723b755649bab001eb0be76bb39ba2bb784
olawale <jao.olaniyi@gmail.com>
2023-01-14 18:24:47 +0100

    search filter component2

--- pages/search.js
+++ pages/search.js
@@ -3,6 +3,8 @@ import { useRouter } from "next/router";
 import Image from "next/image";
 import { Flex, Box, Text, Icon } from "@chakra-ui/react";
 import { BsFilter } from "react-icons/bs";
+import SearchFilters from "../components/SearchFilters";
+import Property from "../components/Property";
 
 const Search = () => {
   const { searchFilters, setSearchFilters } = useState(false);
@@ -22,7 +24,29 @@ const Search = () => {
         <Text>Search Property by Filters</Text>
         <Icon paddingLeft="2" w="8" as={BsFilter} />
       </Flex>
-      {searchFilters && <searchFilters />}
+      {searchFilters && <SearchFilters />}
+      <Text fontSize="2xl" p="4" fontWeight="bold">
+        Properties {router.query.purpose}
+      </Text>
+      <Flex flexWrap="wrap">
+        {properties.map((property) => (
+          <Property property={property} key={property.id} />
+        ))}
+      </Flex>
+      {properties.length === 0 && (
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          flexDir="column"
+          marginTop="5"
+          marginBottom="5"
+        >
+          <Image src={noresult} />
+          <Text fontSize="xl" marginTop="3">
+            No Result Found.
+          </Text>
+        </Flex>
+      )}
     </Box>
   );
 };
