fab3590f775457e2168f81061bb35cf55ceeae15
olawale <jao.olaniyi@gmail.com>
2023-01-14 19:07:27 +0100

    search filter component4

--- pages/search.js
+++ pages/search.js
@@ -3,32 +3,35 @@ import { useRouter } from "next/router";
 import Image from "next/image";
 import { Flex, Box, Text, Icon } from "@chakra-ui/react";
 import { BsFilter } from "react-icons/bs";
-import SearchFilters from "../components/SearchFilters";
 import Property from "../components/Property";
+import SearchFilters from "../components/SearchFilters";
 import { baseUrl, fetchApi } from "../utils/fetchApi";
 import noresult from "../assets/noresult.svg";
 
 const Search = ({ properties }) => {
-  const { searchFilters, setSearchFilters } = useState(false);
-  const router = useRouter;
+  const [searchFilters, setSearchFilters] = useState(false);
+  const router = useRouter();
+
   return (
     <Box>
       <Flex
+        onClick={() => setSearchFilters(!searchFilters)}
         cursor="pointer"
-        bg="gray.200"
-        alignItems="center"
+        bg="gray.100"
+        borderBottom="1px"
+        borderColor="gray.200"
         p="2"
+        fontWeight="black"
         fontSize="lg"
         justifyContent="center"
-        fontWeight="bold"
-        onClick={() => setSearchFilters(!searchFilters)}
+        alignItems="center"
       >
-        <Text>Search Property by Filters</Text>
-        <Icon paddingLeft="2" w="8" as={BsFilter} />
+        <Text>Search Property By Filters</Text>
+        <Icon paddingLeft="2" w="7" as={BsFilter} />
       </Flex>
       {searchFilters && <SearchFilters />}
-      <Text fontSize="2xl" p="4" fontWeight="bold">
-        Properties {router.query}
+      <Text fontSize="2xl" p="4" fontWeight="bold" bg="gray">
+        Properties {router.query.purpose}
       </Text>
       <Flex flexWrap="wrap">
         {properties.map((property) => (
