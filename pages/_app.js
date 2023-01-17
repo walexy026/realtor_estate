5507b79c985394c53db2d9f75dc18552e9dd8eaf
olawale <jao.olaniyi@gmail.com>
2023-01-17 16:03:35 +0100

    scroll bar added

--- pages/_app.js
+++ pages/_app.js
@@ -4,16 +4,25 @@ import Head from "next/head";
 import NProgress from "nprogress";
 import Layout from "../components/Layout";
 export default function App({ Component, pageProps }) {
+  NProgress.configure({ showSpinner: false });
+
+  Router.events.on("routeChangeStart", () => {
+    NProgress.start();
+  });
+
+  Router.events.on("routeChangeComplete", () => {
+    NProgress.done();
+  });
   return (
     <>
       <Head>
-        {/* <link
+        <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
           integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
           crossOrigin="anonymous"
           referrerPolicy="no-referrer"
-        /> */}
+        />
       </Head>
       <ChakraProvider>
         <Layout>
